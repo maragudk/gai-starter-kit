b44e4dc313aa8e25a98f9af710433ee2c66e5d79
Markus Wüstenberg <markus@maragu.dk>
2025-03-11 14:14:13 +0100

    Add `ai.Client.EmbedString`

--- ai/embed.go
+++ ai/embed.go
@@ -2,11 +2,12 @@ package ai
 
 import (
 	"context"
+	"strings"
 
 	"maragu.dev/gai"
 )
 
-// Embed a string to a binary vector.
+// Embed to a binary vector.
 // See https://huggingface.co/blog/embedding-quantization for details on binary embeddings.
 func (c *Client) Embed(ctx context.Context, req gai.EmbedRequest) (gai.EmbedResponse[int], error) {
 	res, err := c.embedder.Embed(ctx, req)
@@ -27,3 +28,14 @@ func (c *Client) Embed(ctx context.Context, req gai.EmbedRequest) (gai.EmbedResp
 }
 
 var _ gai.Embedder[int] = (*Client)(nil)
+
+func (c *Client) EmbedString(ctx context.Context, s string) ([]int, error) {
+	res, err := c.Embed(ctx, gai.EmbedRequest{
+		Input: strings.NewReader(s),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return res.Embedding, nil
+}
